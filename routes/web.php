9b31140420a9e3fcc22e57273caa5c665015c43b
WeiYouYiLin <1450580734@qq.com>
2018-12-20 09:02:07 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]); 等价于
+
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
