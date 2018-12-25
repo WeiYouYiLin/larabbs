4638e2973025bca4ddfcfe10e3647501a57f33d6
WeiYouYiLin <1450580734@qq.com>
2018-12-25 07:15:12 +0000

    WYSIWYG Editor

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -13,6 +13,9 @@ class TopicRequest extends Request
             {
                 return [
                     // CREATE ROLES
+                    'title'       => 'required|min:2',
+                    'body'        => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             // UPDATE
