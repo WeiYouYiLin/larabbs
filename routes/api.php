3b22df203eea5127bd3a3b674c17a28b0a09bae2
WeiYouYiLin <1450580734@qq.com>
2019-01-14 03:39:46 +0000

    topics store

--- routes/api.php
+++ routes/api.php
@@ -39,9 +39,7 @@ $api->version('v1', [
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
-    ], function($api) {
-    	// 游客可以访问的接口
-    	
+    ], function($api) {   	
 	    // 短信验证码
 	    $api->post('verificationCodes', 'VerificationCodesController@store')
 	        ->name('api.verificationCodes.store');
@@ -64,18 +62,27 @@ $api->version('v1', [
 		$api->delete('authorizations/current', 'AuthorizationsController@destroy')
 		    ->name('api.authorizations.destroy');
 
+	    // 游客可以访问的接口
+	 
+    	// 话题分类首页
+    	$api->get('categories', 'CategoriesController@index')->name('api.categories.index');
+    	
 	    // 需要 token 验证的接口
 	    
         $api->group(['middleware' => 'api.auth'], function($api) {
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')
                 ->name('api.user.show');
+             // 编辑登录用户信息
+			$api->patch('user', 'UsersController@update')
+    			->name('api.user.update');
+        	// 图片资源
+    		$api->post('images', 'ImagesController@store')
+        		->name('api.images.store');
+    		// 发布话题
+			$api->post('topics', 'TopicsController@store')
+			    ->name('api.topics.store');
         });
-        // 编辑登录用户信息
-		$api->patch('user', 'UsersController@update')
-    	->name('api.user.update');
-        // 图片资源
-    	$api->post('images', 'ImagesController@store')
-        	->name('api.images.store');
+       
     });
 });
