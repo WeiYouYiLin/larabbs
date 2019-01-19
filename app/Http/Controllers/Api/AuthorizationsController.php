d5db5a1b5e4a20f9deb2c9a5074b2d6ea0406f8c
WeiYouYiLin <1450580734@qq.com>
2019-01-19 08:56:20 +0000

    social login change to passport

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -10,9 +10,11 @@ use Zend\Diactoros\Response as Psr7Response;
 use Psr\Http\Message\ServerRequestInterface;
 use League\OAuth2\Server\Exception\OAuthServerException;
 use League\OAuth2\Server\AuthorizationServer;
+use App\Traits\PassportToken;
 
 class AuthorizationsController extends Controller
 {
+    use PassportToken;
     // 第三方登录
     public function socialStore($type, SocialAuthorizationRequest $request)
     {
@@ -61,9 +63,12 @@ class AuthorizationsController extends Controller
 
                 break;
         }
-
+        /*// 
         $token = Auth::guard('api')->fromUser($user);
-        return $this->respondWithToken($token)->setStatusCode(201);
+        return $this->respondWithToken($token)->setStatusCode(201);*/
+        // 
+        $result = $this->getBearerTokenByUser($user, '1', false);
+        return $this->response->array($result)->setStatusCode(201);
     }
     // 用户登录
     /*public function store(AuthorizationRequest $request)
